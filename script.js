201180e5099b8ebf507a27796a486670fc325f2b
Kamil Mathea <kamilmathe@gmail.com>
2026-04-08 11:15:15 +0200

    feat: fetch and safe detailed pokemon data in global array

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
-let allPokemon = [];
+let allPokemons = [];
 
 async function init() {
     toggleLoadingScreen();
     try {
         await getPokemonList();
+        console.log("Alle Details sind da:", allPokemons);
     } catch (error) {
         console.error("Fehler beim Laden:", error);
     }
@@ -19,5 +20,19 @@ function toggleLoadingScreen() {
 async function getPokemonList() {
     let response = await fetch(BASE_URL + "?limit=20&offset=0");
     let responseToJson = await response.json();
+    let pokemonArray = responseToJson.results;
+
     console.log("Die ersten 20 Pokemon:", responseToJson.results);
+
+    for (let i = 0; i < pokemonArray.length; i++) {
+        let pokemonUrl = pokemonArray[i].url;
+        await getPokemonDetails(pokemonUrl);
+    }
+}
+
+async function getPokemonDetails(url) {
+    let response = await fetch(url);
+    let pokemonDetails = await response.json();
+    
+    allPokemons.push(pokemonDetails);
 }
